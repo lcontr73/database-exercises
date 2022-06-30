ed02afc95eb762ed3fdfec27d8493b09d7102cc3
Leonard Contreras <leonard.contreras03@gmail.com>
2022-06-30 15:45:04 -0500

    Refactored Select statements to select all data from albums

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,14 +1,20 @@
 USE codeup_test_db;
 
 
+# The name of all albums by Pink Floyd.
 SELECT name FROM albums WHERE artist ='Pink Floyd';
 
+# The year Sgt. Pepper's Lonely Hearts Club Band was released
 SELECT release_date FROM albums WHERE name = 'Sgt. Peppers Lonely Hearts Club Band';
 
+# The genre for Nevermind
 SELECT genre FROM albums WHERE name = 'Nevermind';
 
-SELECT name FROM albums WHERE release_date BETWEEN 1990 AND 1999;
+# Which albums were released in the 1990s
+SELECT * FROM albums WHERE release_date BETWEEN 1990 AND 1999;
 
-SELECT name FROM albums WHERE sales < 20;
+# Which albums had less than 20 million certified sales
+SELECT * FROM albums WHERE sales < 20;
 
-SELECT name FROM albums WHERE genre = 'Rock';
+# All the albums with a genre of "Rock". Why do these query results not include albums with a genre of "Hard rock" or "Progressive rock"?
+SELECT * FROM albums WHERE genre = 'Rock';
